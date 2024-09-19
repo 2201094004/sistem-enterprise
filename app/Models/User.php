ad9161454bf6153b173e7f64a7453a5da0939f50
unknown <syaviraindahmaryam@gmail.com>
2024-09-19 16:04:20 +0700

    dua commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role_id', // Tambahkan role_id
+        
     ];
     
 
@@ -45,10 +45,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    public function role()
-{
-    return $this->belongsTo(Role::class);
-}
-
 }
